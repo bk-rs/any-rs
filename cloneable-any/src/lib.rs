3003a4a63951c71bd3af8466d3bd973d4d4b388c
vkill <vkill.net@gmail.com>
2022-01-19 14:35:59 +0800

    Update cloneable-any

--- cloneable-any/src/lib.rs
+++ cloneable-any/src/lib.rs
@@ -1,1 +1,56 @@
+#![cfg_attr(not(feature = "std"), no_std)]
 
+use core::fmt;
+
+use downcast_rs::{impl_downcast, Downcast, DowncastSync};
+use dyn_clone::{clone_trait_object, DynClone};
+
+//
+//
+//
+pub trait CloneableAny: Downcast + DynClone {}
+
+impl_downcast!(CloneableAny);
+clone_trait_object!(CloneableAny);
+
+impl fmt::Debug for (dyn CloneableAny) {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_tuple("CloneableAny")
+            .field(&format_args!("_"))
+            .finish()
+    }
+}
+impl fmt::Debug for (dyn CloneableAny + Send) {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_tuple("CloneableAny")
+            .field(&format_args!("_"))
+            .finish()
+    }
+}
+impl fmt::Debug for (dyn CloneableAny + Send + Sync) {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_tuple("CloneableAny")
+            .field(&format_args!("_"))
+            .finish()
+    }
+}
+
+impl<T> CloneableAny for T where T: 'static + Clone {}
+
+//
+//
+//
+pub trait CloneableAnySync: DowncastSync + DynClone {}
+
+impl_downcast!(CloneableAnySync);
+clone_trait_object!(CloneableAnySync);
+
+impl fmt::Debug for (dyn CloneableAnySync) {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_tuple("CloneableAnySync")
+            .field(&format_args!("_"))
+            .finish()
+    }
+}
+
+impl<T> CloneableAnySync for T where T: 'static + Clone + Send + Sync {}
